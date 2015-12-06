c3a04a86d1283aeca2be003a9b99692ecd4a391b
Andy Leeds <aleeds444@gmail.com>
2015-12-06 17:06:05 -0600

    Filled in the live_coding, added, left template

--- live_coding_complete.py
+++ live_coding_complete.py
@@ -22,6 +22,19 @@ def PlayAverage(num_players,avg):
     return 2 * avg * (num_players - 1) / (3 * num_players - 2)
 
 # This file was created for a live coding demo for my Game Theory class.
-  
+def Respond(lmbda,num_players,level):
+    if level == 0:
+        return (50,[50])
+    else:
+        probs = NormalizedPoisson(lmbda,level)
+        (a,responses) = Respond(lmbda,num_players,level - 1)
+        avg = sum([probs[i] * responses[i] for i in range(0,level)])
+        play = PlayAverage(num_players,avg)
+        return (play,responses + [play])
 
 def PlayWithPlayers(num_players,lmbda):
+    ls = []
+    for i in range(0,num_players):
+        (a,b) = Respond(lmbda,num_players,AssignLevel(lmbda,num_players))
+        ls = ls + [a]
+    return sum(ls) / len(ls)
